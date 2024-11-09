e6d5a8eef8d1391162de7017847b57c6fb9a5ae6
Muslihah-A <ceciyaceciy@gmail.com>
2024-11-09 23:43:31 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,61 +1,29 @@
-<main>
-    <div class="container-fluid px-4">
-        <h1 class="mt-4"></h1>
-        <ol class="breadcrumb mb-4">
-        <li class="breadcrumb-item"><a href="<?php echo site_url('barang') ?>">Barang</a></li>
-        </ol>
-        <div class="card mb-4">
-            <div class="card-header">
-                <a href="<?php echo site_url('barang/add')?>"><i class="fas fa-plus"></i> Add New</a>
-            </div>
-            <?php if ($this->session->flashdata('successs')); ?>
-            <div class="alert alert-succes" role="alert">
-                <?php echo $this->session->flashdata('succes'); ?>
-            </div>
-            <?php endif; ?>
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table class="table table-striped table-bordered table-hover" id="tabelkelas" width="100%" cellspacing="0">
-                        <thead>
-                            <tr>
-                                <th>No.</th>
-                                <th>Barkode</th>
-                                <th>Name</th>
-                                <th>Satuan</th>
-                                <th>Kategori</th>
-                                <th>Stok</th>
-                                <th>Harga Beli</th>
-                                <th>Harga Jual</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        <?php
-                        $no = 1;
-                        foreach ($barang as $barang){
-                            echo "<tr>
-                            <td>$no</td>
-                            <td>$barang->barkode</td>
-                            <td>$barang->name</td>
-                            <td>$barang->satuan</td>
-                            <td>$barang->kategori</td>
-                            <td>$barang->stok</td>
-                            <td>$barang->harga_beli</td>
-                            <td>$barang->harga_jual</td>
-                            <td>
-                            <div>
-                                <a href=".base_url('barang/getedit/' . $barang->id)." class='btn btn-sm btn-info'><i class='fas fa-edit'></i>EDIT</a>
-                                <a href='".base_url('barang/delete/'.$barang->id)."' class='btn btn-sm btn-danger' 
-                                            onclick='return confirm(\"Ingin menghapus data user ini?\");'>
-                                                <i class='fas fa-trash'></i> Hapus </a>
-                            </div>
-                            </td>
-                            </tr>"
-                        $no++;
-                        }?>
-                        </tbody>
-                    </table>
-            </div>
-        </div>
-    </div>
-</main>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Data Satuan</title>
+</head>
+<body>
+    <h1>Data Satuan</h1>
+    <a href="<?php echo site_url('satuan/add'); ?>">Tambah Satuan</a>
+    <table border="1" cellpadding="5">
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Deskripsi</th>
+            <th>Aksi</th>
+        </tr>
+        <?php $no = 1; foreach ($satuan as $item): ?>
+        <tr>
+            <td><?php echo $no++; ?></td>
+            <td><?php echo $item->name; ?></td>
+            <td><?php echo $item->deskripsi; ?></td>
+            <td>
+                <a href="<?php echo site_url('satuan/edit/'.$item->id); ?>">Edit</a>
+                <a href="<?php echo site_url('satuan/delete/'.$item->id); ?>" onclick="return confirm('Ingin menghapus data ini?');">Hapus</a>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
